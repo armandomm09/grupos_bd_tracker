137da3d7630404c7b496befd2bae2ba1ebcd5085
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2024-10-10 18:41:13 -0600

    Change url

--- gomailer/main.go
+++ gomailer/main.go
@@ -3,28 +3,53 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/smtp"
+	"time"
+
+	"github.com/Rhymen/go-whatsapp"
 )
 
 func main() {
 
-	from := "pabloarmando.macbeath@gmail.com"
-	pass := "jxyp kmnl ijxv gvpc"
-	to := "binizavazquez@gmail.com"
-
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: Hello there\n\n" +
-		"Hola biniza"
-
-	err := smtp.SendMail("smtp.gmail.com:587",
-		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
-		from, []string{to}, []byte(msg))
-
+	wac, err := whatsapp.NewConn(20 * time.Second)
 	if err != nil {
-		log.Printf("smtp error: %s", err)
-		return
+		log.Fatal(err)
 	}
 
-	fmt.Println("Sent")
+	qrChan := make(chan string)
+	go func() {
+		fmt.Printf("qr code: %v\n", <-qrChan)
+		//show qr code or save it somewhere to scan
+	}()
+
+	sess, err := wac.Login(qrChan)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newSess, err := wac.RestoreWithSession(sess)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(newSess.Wid)
+
+	// from := "pabloarmando.macbeath@gmail.com"
+	// pass := "jxyp kmnl ijxv gvpc"
+	// to := "binizavazquez@gmail.com"
+
+	// msg := "From: " + from + "\n" +
+	// 	"To: " + to + "\n" +
+	// 	"Subject: Hello there\n\n" +
+	// 	"Hola biniza"
+
+	// err := smtp.SendMail("smtp.gmail.com:587",
+	// 	smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+	// 	from, []string{to}, []byte(msg))
+
+	// if err != nil {
+	// 	log.Printf("smtp error: %s", err)
+	// 	return
+	// }
+
+	// fmt.Println("Sent")
 }
