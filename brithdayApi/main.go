2fcb67edf9d6ec7d7ab6ace9dcef2eb7317ed77f
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2024-11-26 16:23:40 -0600

    remove venv

--- brithdayApi/main.go
+++ brithdayApi/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
-	"brithdayApi/database"
 	"brithdayApi/routes"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
+
+func requestLogger() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+
+		// Obtener la dirección IP del cliente
+		clientIP := c.IP()
+
+		// Imprimir detalles de la solicitud
+		fmt.Println("\n- - - - - - - INCOMING REQUEST - - - - - - - -\n")
+		fmt.Printf("Received request: %s %s\n", c.Method(), c.Path())
+		fmt.Printf("Client IP: %s\n", clientIP)
+		fmt.Printf("User Agent: %s\n", c.Get("User-Agent"))
+
+		// Llamar al siguiente middleware o handler
+		err := c.Next()
+
+		fmt.Printf("Completed request: %s %s in %v\n", c.Method(), c.Path(), time.Since(start))
+
+		return err
+	}
+}
 
 func main() {
 	err := godotenv.Load()
@@ -24,9 +47,9 @@ func main() {
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
 	}))
 
-	database.ConnectDB()
-
+	// database.ConnectDB()
+	app.Use(requestLogger())
 	routes.SetPersonRoutes(app)
 
-	log.Fatal(app.Listen(":8000"))
+	log.Fatal(app.Listen(":8888"))
 }
